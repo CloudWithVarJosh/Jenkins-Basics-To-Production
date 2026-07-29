45e89c3799b38ff5783d3ebd6aa8a1cc0afdbb4d
Vinod Kumar Jalagam <63002962+vinodjalagam@users.noreply.github.com>
2026-07-29 17:31:57 +0530

    Change welcome message in app.py

--- Day 07/python/app.py
+++ Day 07/python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.get("/")
 def hello():
     return jsonify(
-        message="✨ Welcome to Cloud with vinod hyderabad ✨",
+        message="✨ Welcome to Cloud with vinod BYE✨",
         tip="Built with Flask, shipped by Jenkins, running in Docker from git."
     )
 
